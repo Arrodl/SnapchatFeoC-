4887976eb640ea06e2c21c6d935187ae25d4bdd2
Arrodl <arrodl40@gmail.com>
2017-10-29 14:15:58 -0600

    Slider para cambiar entre estados

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -1,6 +1,3 @@
-// BoundingObjects.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/videoio.hpp"
@@ -24,7 +21,11 @@ String window_name_face = "Face Detector";
 String window_name_edges = "Edge Detector";
 String imageName = ("lentesteodoro.png");
 
-//Declaramos los metodos
+int thresh = 0;
+int max_thresh = 2;
+RNG rng(12345);
+void thresh_callback(int, void*);
+void MyPolygon(Mat img, int w);
 void applyChangesToFrame(Mat frame);
 void detectFaceInFrame(Mat frame);
 
@@ -45,21 +46,38 @@ int main(int argc, const char** argv)
 	if (!stream.isOpened())  // check if we succeeded
 		return -1;
 	namedWindow(window_name_edges, 1);
-	namedWindow(window_name_face, 1);
-	
+
 	Mat frame;
 
 	while (stream.read(frame)) {
 		if (frame.empty()) {
 			break;
 		}
-		applyChangesToFrame(frame);
-		detectFaceInFrame(frame);
+		createTrackbar(" Threshold:", window_name_edges, &thresh, max_thresh, thresh_callback);
+		switch (thresh)
+		{
+		case 1:
+			applyChangesToFrame(frame);
+			break;
+		case 2:
+			detectFaceInFrame(frame);
+			break;
+		case 0:
+			stream >> frame;
+			cvtColor(frame, edges, COLOR_BGR2GRAY);
+			GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+			imshow(window_name_edges, edges);
+			break;
+		}
 		char c = (char)waitKey(10);
 		if (c == 27)
 			break;
 	}
 	return 0;
+}
+
+void thresh_callback(int, void*) {
+
 }
 
 /// <sumary>
@@ -68,14 +86,13 @@ int main(int argc, const char** argv)
 void applyChangesToFrame(Mat frame) {
 	stream >> frame;
 	cvtColor(frame, edges, COLOR_BGR2GRAY);
-	GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
 	Canny(edges, edges, 0, 30, 3);
 	imshow(window_name_edges, edges);
 }// changeEveryFrame
 
-/// <sumary>
-/// Metodo detectar la cara y sobreponer una imagen
-/// </sumary>
+ /// <sumary>
+ /// Metodo detectar la cara y sobreponer una imagen
+ /// </sumary>
 void detectFaceInFrame(Mat frame) {
 	stream >> frame;
 	std::vector<Rect> faces;
@@ -94,15 +111,47 @@ void detectFaceInFrame(Mat frame) {
 			//Resize image to face
 			Size sizeOfFace(50, 10);
 			resize(image, image, sizeOfFace, 0.0, 0.0, INTER_AREA);
-			cv::Rect roi(cv::Point(0, 0), cv::Size(50, 10));
-			cv::Mat destinationROI = frame_gray(roi);
-			image.copyTo(destinationROI(cv::Rect(0, 0, image.cols, image.rows)));
-			
+
+			MyPolygon(frame_gray, faces[i].width);
 		}
 		//addWeighted(frame_gray, 1.0, image,1.0, 0.0, frame_gray);
 
 		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
 		Mat faceROI = frame_gray(faces[i]);
 	}
-	imshow(window_name_face, frame);
+	imshow(window_name_edges, frame);
+}
+
+void MyPolygon(Mat img, int w)
+{
+	int lineType = LINE_8;
+	Point rook_points[1][20];
+	rook_points[0][0] = Point(w / 4, 7 * w / 8);
+	rook_points[0][1] = Point(3 * w / 4, 7 * w / 8);
+	rook_points[0][2] = Point(3 * w / 4, 13 * w / 16);
+	rook_points[0][3] = Point(11 * w / 16, 13 * w / 16);
+	rook_points[0][4] = Point(19 * w / 32, 3 * w / 8);
+	rook_points[0][5] = Point(3 * w / 4, 3 * w / 8);
+	rook_points[0][6] = Point(3 * w / 4, w / 8);
+	rook_points[0][7] = Point(26 * w / 40, w / 8);
+	rook_points[0][8] = Point(26 * w / 40, w / 4);
+	rook_points[0][9] = Point(22 * w / 40, w / 4);
+	rook_points[0][10] = Point(22 * w / 40, w / 8);
+	rook_points[0][11] = Point(18 * w / 40, w / 8);
+	rook_points[0][12] = Point(18 * w / 40, w / 4);
+	rook_points[0][13] = Point(14 * w / 40, w / 4);
+	rook_points[0][14] = Point(14 * w / 40, w / 8);
+	rook_points[0][15] = Point(w / 4, w / 8);
+	rook_points[0][16] = Point(w / 4, 3 * w / 8);
+	rook_points[0][17] = Point(13 * w / 32, 3 * w / 8);
+	rook_points[0][18] = Point(5 * w / 16, 13 * w / 16);
+	rook_points[0][19] = Point(w / 4, 13 * w / 16);
+	const Point* ppt[1] = { rook_points[0] };
+	int npt[] = { 20 };
+	fillPoly(img,
+		ppt,
+		npt,
+		1,
+		Scalar(255, 255, 255),
+		lineType);
 }
