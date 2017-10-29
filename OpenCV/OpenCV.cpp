c8aa02bd1905a6029f76136dfa3251465b838f71
MarianaPalafox <30907314+MarianaPalafox@users.noreply.github.com>
2017-10-28 21:48:00 -0500

    Se añade el proyecto

---
+++ OpenCV/OpenCV.cpp
@@ -0,0 +1,108 @@
+// BoundingObjects.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+// Declaramos las variables
+
+// Abrimos un stream para la captura de video
+VideoCapture stream(0);
+Mat edges;
+String face_cascade_name, eyes_cascade_name;
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+String window_name_face = "Face Detector";
+String window_name_edges = "Edge Detector";
+String imageName = ("lentesteodoro.png");
+
+//Declaramos los metodos
+void applyChangesToFrame(Mat frame);
+void detectFaceInFrame(Mat frame);
+
+int main(int argc, const char** argv)
+{
+	CommandLineParser parser(argc, argv,
+		"{help h||}"
+		"{face_cascade|haarcascades/haarcascade_frontalface_alt.xml|}"
+		"{eyes_cascade|haarcascades/haarcascade_eye_tree_eyeglasses.xml|}");
+	parser.printMessage();
+	face_cascade_name = "haarcascades/haarcascade_frontalface_alt.xml";
+	eyes_cascade_name = "haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+	//-- 1. Load the cascades
+	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
+	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
+
+	parser.printMessage();
+	if (!stream.isOpened())  // check if we succeeded
+		return -1;
+	namedWindow(window_name_edges, 1);
+	namedWindow(window_name_face, 1);
+	
+	Mat frame;
+
+	while (stream.read(frame)) {
+		if (frame.empty()) {
+			break;
+		}
+		applyChangesToFrame(frame);
+		detectFaceInFrame(frame);
+		char c = (char)waitKey(10);
+		if (c == 27)
+			break;
+	}
+	return 0;
+}
+
+/// <sumary>
+/// Metodo para aplicar blur y deteccion de lineas a imagen
+/// </sumary>
+void applyChangesToFrame(Mat frame) {
+	stream >> frame;
+	cvtColor(frame, edges, COLOR_BGR2GRAY);
+	GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+	Canny(edges, edges, 0, 30, 3);
+	imshow(window_name_edges, edges);
+}// changeEveryFrame
+
+/// <sumary>
+/// Metodo detectar la cara y sobreponer una imagen
+/// </sumary>
+void detectFaceInFrame(Mat frame) {
+	stream >> frame;
+	std::vector<Rect> faces;
+	Mat frame_gray;
+	Mat image;
+	image = imread(imageName, IMREAD_UNCHANGED);
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point2i center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+
+		if (!image.empty()) {
+			//Resize image to face
+			Size sizeOfFace(50, 10);
+			resize(image, image, sizeOfFace, 0.0, 0.0, INTER_AREA);
+			cv::Rect roi(cv::Point(0, 0), cv::Size(50, 10));
+			cv::Mat destinationROI = frame_gray(roi);
+			image.copyTo(destinationROI(cv::Rect(0, 0, image.cols, image.rows)));
+			
+		}
+		//addWeighted(frame_gray, 1.0, image,1.0, 0.0, frame_gray);
+
+		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		Mat faceROI = frame_gray(faces[i]);
+	}
+	imshow(window_name_face, frame);
+}
